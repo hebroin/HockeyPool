59da64998bd67c58362f9efcac319408c2207fff
hebroin <bpg_85@hotmail.com>
2015-04-12 19:58:42 -0400

    First commit

---
+++ slnHockeyPool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="slnHockeyPool.MvcApplication" Language="C#" %>
---
+++ slnHockeyPool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(slnHockeyPool.Startup))]
+namespace slnHockeyPool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
